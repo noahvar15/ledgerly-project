68727de2479496ea5ddb28b4d8dcefe49bdcfa7e
Noah Vargas <noahvar23@gmail.com>
2025-03-25 12:04:16 -0400

    revamped index.html left-sidebar

--- ledgerly/dashboard/urls.py
+++ ledgerly/dashboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index, name='dashboard-home'),
+    path('', views.index, name='dashboard-home'),
     path('analytics/', views.analytics, name='dashboard-analytics'),
 ]

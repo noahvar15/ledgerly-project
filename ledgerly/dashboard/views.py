109c2b0f75c4e4acd3203e63a29b4023d3644a2c
Noah Vargas <noahvar23@gmail.com>
2025-04-02 12:06:55 -0400

    all dashboard views redirect you to login page if you are not logged in

--- ledgerly/dashboard/views.py
+++ ledgerly/dashboard/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def index(request):
     return render(request, 'dashboard/index.html', {})
 
+@login_required
 def analytics(request):
     return render(request, 'dashboard/analytics.html', {})
 
+@login_required
 def assets(request):
     return render(request, 'dashboard/assets.html', {})
 
+@login_required
 def transactions(request):
     return render(request, 'dashboard/transactions.html', {})
 
+@login_required
 def settings(request):
     return render(request, 'dashboard/dashboard-settings.html', {})

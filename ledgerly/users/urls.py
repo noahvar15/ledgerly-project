2b5abcafdc203a77ef8edcb691770acbc1bcf4b4
Noah Vargas <noahvar23@gmail.com>
2025-04-02 11:49:28 -0400

    logout button sends POST request to log user out

--- ledgerly/users/urls.py
+++ ledgerly/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views 
 from . import views
 
 urlpatterns = [
     path('', views.logIn, name='login'),
     path('signup', views.signUp, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
